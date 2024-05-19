d0ebaf6f5d9988dbd479bcb1ca858862a2a69004
0xF76 <jakub.bubak03@gmail.com>
2024-05-19 10:08:50 +0200

    minor fix

--- master_v3.1/master_lib/commands/uart/uart_cmd_arm_6dof.c
+++ master_v3.1/master_lib/commands/uart/uart_cmd_arm_6dof.c
@@ -249,7 +249,7 @@ void Cmd_UART_Arm6DOF_GetGripper() {
 void Cmd_UART_ARM_CM4(uint8_t* data, uart_packet_link_t link_type) {
     uart_packet_t msg = {
             .cmd = UART_CMD_ARM_CM4,
-            .arg_count = UART_ARG_ARM_CM4
+            .arg_count = UART_ARG_ARM_CM4,
             .origin = LINK_RS422_UART
     };
 
